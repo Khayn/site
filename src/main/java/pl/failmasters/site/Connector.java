d9dc74146439c252d3edb8f099cdeb8792f2cce2
Mateusz <Mateusz@Apollo>
2019-02-24 21:17:38 +0100

    prepared statements

--- src/main/java/pl/failmasters/site/Connector.java
+++ src/main/java/pl/failmasters/site/Connector.java
@@ -2,6 +2,7 @@ package pl.failmasters.site;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -10,65 +11,104 @@ public class Connector {
 
 	public static final String CONNECTION_STR = "jdbc:mysql://khayn.synology.me:5306/site";
 	public static final String DB_USER = "root";
-	public static final String DB_PASSWORD = "crd123!";
+	public static final String DB_PWD = "crd123!";
 
-	public static final String insertQuery =
-			"INSERT INTO 'users' ('name', 'surename', 'email', 'password') VALUES ('Magda', 'Dzik', 'dzik@abc.pl', 'haslo123');";
-
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) {
 		Connector conn = new Connector();
-		Connection connection = DriverManager.getConnection(CONNECTION_STR, DB_USER, DB_PASSWORD);
+		Connection connection = null;
 
-		conn.printUserTable(connection);
-		conn.insertUser(connection, insertQuery);
+		try {
+			connection = DriverManager.getConnection(CONNECTION_STR, DB_USER, DB_PWD);
 
-		conn.updateUserSurename(connection, "?bik", "Dzik");
+			conn.printUserTable(connection);
 
-		conn.printUserTable(connection);
+			conn.insertUser(connection);
+			conn.updateUserSurename(connection, "Dzik", "Kot");
+			conn.deleteUser(connection, "Kot");
 
-		conn.deleteUser(connection, "Magda");
+			conn.printUserTable(connection);
+
+		} catch (SQLException e) {
+
+		}
+
 	}
 
-	public void printUserTable(final Connection connection) throws SQLException {
-		Statement statement = connection.createStatement();
-		ResultSet result = statement.executeQuery("select * from users order by surename");
+	public void printUserTable(final Connection connection) {
+		Statement statement = null;
+		ResultSet result = null;
 
-		while (result.next()) {
-			for (Columns dbColumn : Columns.values()) {
-				String dbColumnName = dbColumn.getDbColumnName();
-				System.out.printf("Column %s : %s \n", dbColumnName, result.getString(dbColumnName));
+		try {
+			statement = connection.createStatement();
+			result = statement.executeQuery("SELECT * FROM users order by surename");
+
+			while (result.next()) {
+				for (Columns dbColumn : Columns.values()) {
+					String dbColumnName = dbColumn.getDbColumnName();
+					System.out.printf("Column %s : %s \n", dbColumnName, result.getString(dbColumnName));
+				}
+
+				System.out.println();
 			}
 
-			System.out.println();
+		} catch (SQLException e) {
+
 		}
 	}
 
-	public void insertUser(final Connection connection, final String query) throws SQLException {
-		Statement statement = connection.createStatement();
+	public void insertUser(final Connection connection) {
+		PreparedStatement statement = null;
 
-		int affectedRows = statement.executeUpdate(query);
-		System.out.printf("affected rows: %d \n", affectedRows);
+		try {
+			String insertQuery = "INSERT INTO users (name, surename, email, password) VALUES (?, ?, ?, ?)";
+			statement = connection.prepareStatement(insertQuery);
+			statement.setString(1, "Magda");
+			statement.setString(2, "Dzik");
+			statement.setString(3, "abc@test.pl");
+			statement.setString(4, "haslo123");
 
+			int affectedRows = statement.executeUpdate();
+			System.out.printf("insert affected rows: %d \n\n", affectedRows);
+
+		} catch (SQLException e) {
+
+		}
 	}
 
-	public void updateUserSurename(final Connection connection, final String previousSurename, final String newSurename)
-			throws SQLException {
-		Statement statement = connection.createStatement();
+	public void updateUserSurename(final Connection connection, final String previousSurename,
+			final String newSurename) {
+		PreparedStatement statement = null;
 
-		String query = "update users set surename='" + newSurename + "' where surename='" + previousSurename + "'";
-		int affectedRows = statement.executeUpdate(query);
+		try {
+			String query = "UPDATE users SET surename=? WHERE surename=?";
+			statement = connection.prepareStatement(query);
+			statement.setString(1, newSurename);
+			statement.setString(2, previousSurename);
 
-		System.out.printf("affected rows: %d \n", affectedRows);
+			int affectedRows = statement.executeUpdate();
 
+			System.out.printf("update affected rows: %d \n\n", affectedRows);
+
+		} catch (SQLException e) {
+
+		}
 	}
 
-	public void deleteUser(final Connection connection, final String surename) throws SQLException {
-		Statement statement = connection.createStatement();
+	public void deleteUser(final Connection connection, final String surename) {
+		PreparedStatement statement = null;
 
-		String query = "delete from users where surename='" + surename + "'";
-		int affectedRows = statement.executeUpdate(query);
+		try {
+			String query = "delete from users where surename=?";
+			statement = connection.prepareStatement(query);
+			statement.setString(1, surename);
 
-		System.out.printf("affected rows: %d \n", affectedRows);
+			int affectedRows = statement.executeUpdate();
+
+			System.out.printf("delete affected rows: %d \n\n", affectedRows);
+
+		} catch (SQLException e) {
+
+		}
 	}
 
 }
